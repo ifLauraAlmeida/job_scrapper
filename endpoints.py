6a736c83b407be5c2c9b1cf727a8aca52e4214c6
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-03-22 17:50:33 -0300

    feat: create endpoints.py to modularize web scraping URLs

--- endpoints.py
+++ endpoints.py
@@ -1,7 +1,7 @@
 urls = {
-    "remotar": {"url_q"="https://remotar.com.br/search/jobs?q=", "active":1},
-    "weworkremotely": {"url_q"="https://weworkremotely.com/remote-jobs/search?term=", "active":1},
-    "skipthedrive": {"url_q"="https://skipthedrive.com/?s=","active":1},
-    "workingnomads": {"url_q"="https://workingnomads.com/jobs?tag=", "active":1},
-    "remotar": {"url_q"="https://remotar.com.br/search/jobs?q=", "active":0},
+    "remotar": {"url_q" : "https://remotar.com.br/search/jobs?q=", "active":1},
+    "weworkremotely": {"url_q": "https://weworkremotely.com/remote-jobs/search?term=", "active":1},
+    "skipthedrive": {"url_q" : "https://skipthedrive.com/?s=","active":1},
+    "workingnomads": {"url_q" : "https://workingnomads.com/jobs?tag=", "active":1},
+    "vagas": {"url_q" : "https://vagas.com.br/vagas-", "active":0},
 }
