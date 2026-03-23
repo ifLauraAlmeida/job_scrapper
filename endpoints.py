3c6029a42a9bf2c56f736afbb11dd53451966d91
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-03-22 23:24:53 -0300

    fix(endpoints): deactivate sites with non-200 status or URL pattern

--- endpoints.py
+++ endpoints.py
@@ -1,7 +1,7 @@
 urls = {
     "remotar": {"url_q" : "https://remotar.com.br/search/jobs?q=", "active":1},
-    "weworkremotely": {"url_q": "https://weworkremotely.com/remote-jobs/search?term=", "active":1},
-    "skipthedrive": {"url_q" : "https://skipthedrive.com/?s=","active":1},
+    "weworkremotely": {"url_q": "https://weworkremotely.com/remote-jobs/search?term=", "active":0}, #status code 403
+    "skipthedrive": {"url_q" : "https://skipthedrive.com/?s=","active":0}, #status code 403
     "workingnomads": {"url_q" : "https://workingnomads.com/jobs?tag=", "active":1},
-    "vagas": {"url_q" : "https://vagas.com.br/vagas-", "active":0},
+    "vagas": {"url_q" : "https://vagas.com.br/vagas-", "active":0}, #endpoint diferente do padrão
 }
