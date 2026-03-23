9b49661b6847069e46ed2ffbf2dc40f5636b8c3c
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-03-22 23:25:34 -0300

    fix(extract): resolve various bugs in the extraction module

--- methods/extract.py
+++ methods/extract.py
@@ -1,25 +1,25 @@
 
 import requests
-from utils import utils
 
 class Extract:
-    def __init__(self, urls, path, date):
+    def __init__(self, urls, date, utils,  path = "lake"):
         self.urls   = urls
         self.path   = path
         self.year   = date["year"]
         self.month  = date["month"]
         self.day    = date["day"]
-        self.utils  = Utils()
+        self.utils  = utils
     
         #Extração de Dados
     def extractData(self, query = "data engineer"):
-        for site, data in urls.items():
+        for site, data in self.urls.items():
             endpoint = data["url_q"] + query.replace(" ","+") 
             print(endpoint)
-            self.utils.createDir(f"{self.path}/{self.year}/{self.month}/{self.day}/{site}")
+            if data["active"] == 1:
+                self.utils.createDir(f"{self.path}/{self.year}/{self.month}/{self.day}/{site}")
 
             html_response = requests.get(endpoint)
-            if html_response.status_code == 200:
+            if html_response.status_code == 200 and data["active"] == 1:
                 file_name_path = f"{self.path}/{self.year}/{self.month}/{self.day}/{site}/{query.replace(" ","_")}.html"
                 with open(file_name_path, "w") as f:
                     f.write(html_response.text)
